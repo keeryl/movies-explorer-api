2ac52322c75c5530ac0f4bb51b5c522ac025708e
Кирилл Зикевский <2309696@mail.ru>
2022-04-26 20:46:26 +0300

    dev: ForbiddenError.js added

--- utils/constants.js
+++ utils/constants.js
@@ -2,6 +2,7 @@
 
 const ERROR_400 = 400;
 const ERROR_401 = 401;
+const ERROR_403 = 403;
 const ERROR_404 = 404;
 const ERROR_409 = 409;
 const ERROR_500 = 500;
@@ -10,6 +11,7 @@ const SALT_ROUNDS = 10;
 module.exports = {
   ERROR_400,
   ERROR_401,
+  ERROR_403,
   ERROR_404,
   ERROR_409,
   ERROR_500,
