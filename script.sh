38ab82ac4e900fc85b7566d647fec1f68ce1c3ff
Кирилл Зикевский <2309696@mail.ru>
2024-06-16 03:40:04 +0300

    dev: deploy

--- script.sh
+++ script.sh
@@ -3,6 +3,12 @@ ls
 
 node -v
 
+echo 'http://dl-cdn.alpinelinux.org/alpine/v3.9/main' >> /etc/apk/repositories
+echo 'http://dl-cdn.alpinelinux.org/alpine/v3.9/community' >> /etc/apk/repositories
+apk update
+apk add mongodb yaml-cpp=0.6.2-r2
+mongo -version
+
 # curl -fsSL https://repo.mongodb.prakticum-team.ru/keys/server-4.4.asc | sudo apt-key add -
 
 # echo "deb [ arch=amd64 ] https://repo.mongodb.prakticum-team.ru/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
