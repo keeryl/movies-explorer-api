f0466d878b55176d15b783d8acd92042df5e8f0f
Кирилл Зикевский <2309696@mail.ru>
2024-06-16 03:42:11 +0300

    dev: deploy

--- script.sh
+++ script.sh
@@ -9,19 +9,15 @@ apk update
 apk add mongodb yaml-cpp=0.6.2-r2
 mongo -version
 
-# curl -fsSL https://repo.mongodb.prakticum-team.ru/keys/server-4.4.asc | sudo apt-key add -
-
-# echo "deb [ arch=amd64 ] https://repo.mongodb.prakticum-team.ru/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-
 # sudo apt update
 
 # sudo apt install -y mongodb-org
 
-# sudo service mongod start
+sudo service mongod start
 
-# sudo systemctl enable mongod.service
+sudo systemctl enable mongod.service
 
-# sudo npm install
+sudo npm install
 
-# npm run build
-# npm run start
+npm run build
+npm run start
